d5cae218c88096cc214f128abbd3c8241e908d23
John Caleb <Calebjohn3112@gmail.com>
2025-03-28 22:37:22 +0100

    setup welcome page screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,33 @@ import { useCallback, useEffect, useState } from '@lynx-js/react'
 
 import './App.css'
 import arrow from './assets/arrow.png'
-import lynxLogo from './assets/lynx-logo.png'
+import unikoinLogo from './assets/unikoin-logo.png'
 import reactLynxLogo from './assets/react-logo.png'
 
 export function App() {
-  const [alterLogo, setAlterLogo] = useState(false)
 
   useEffect(() => {
     console.info('Hello, ReactLynx')
   }, [])
-
-  const onTap = useCallback(() => {
-    'background only'
-    setAlterLogo(!alterLogo)
-  }, [alterLogo])
 
   return (
     <view>
       <view className='Background' />
       <view className='App'>
         <view className='Banner'>
-          <view className='Logo' bindtap={onTap}>
-            {alterLogo
-              ? <image src={reactLynxLogo} className='Logo--react' />
-              : <image src={lynxLogo} className='Logo--lynx' />}
+          <view className='Logo'>
           </view>
-          <text className='Title'>Unikoin</text>
         </view>
         <view className='Content'>
-          <image src={arrow} className='Arrow' />
-          <text className='Description'>Welcome to Unikoin!</text>
+          <image src={unikoinLogo} className='Arrow' />
+          <text className='Description'>Welcome to unikoin!</text>
           <text className='Hint'>
             Efforrtlessly manage events that inspire communities.
           </text>
         </view>
-        <view style={{ flex: 1 }}></view>
+        <view className='Footer'>
+          <text class='text-center'>Get Started</text>
+        </view>
       </view>
     </view>
   )
